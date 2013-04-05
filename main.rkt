9950df1fee879011389e164a35c59ccdf6e891fa
Asumu Takikawa <asumu@ccs.neu.edu>
2013-04-05 11:38:49 -0400

    Make it work for forms that expand to definitions
    For example, `define/contract` expands to a `begin`
    with a sequence of definitions. This was mistaken
    as an expression before, but should be handled
    correctly now.

--- main.rkt
+++ main.rkt
@@ -9,19 +9,24 @@
   (syntax-parse stx
     ([_ . form]
      (define expanded-stx (local-expand #'form 'top-level #f))
-     (syntax-parse expanded-stx
-       [(~and form (kw:identifier e:expr ...))
-        #:when (for/or ([k (list #'#%plain-lambda #'case-lambda #'if #'begin #'begin0
-                                 #'let-values #'letrec-values #'set! #'quote #'quote-syntax
-                                 #'with-continuation-mark #'#%plain-app #'#%top
-                                 #'#%variable-reference #'#%expression)])
-                 (free-identifier=? k #'kw))
-        #'(let ([val form])
-            (cond [(has-contract? val)
-                   (define ctc-msg
-                     (format "- : ~a" (contract-name (value-contract val))))
-                   (displayln ctc-msg)
-                   val]
-                  [else val])
-                val)]
-       [form #'form]))))
+     (define (transform stx)
+       (syntax-parse stx
+         [((~literal begin) e:expr ... e1:expr)
+          #`(begin e ... #,(transform #'e1))]
+         [id:identifier (transform #'(#%expression id))]
+         [(~and form (kw:identifier e:expr ...))
+          #:when (for/or ([k (list #'#%plain-lambda #'case-lambda #'if #'begin #'begin0
+                                   #'let-values #'letrec-values #'set! #'quote #'quote-syntax
+                                   #'with-continuation-mark #'#%plain-app #'#%top
+                                   #'#%variable-reference #'#%expression)])
+                   (free-identifier=? k #'kw))
+          #'(let ([val form])
+              (cond [(has-contract? val)
+                     (define ctc-msg
+                       (format "- : ~a" (contract-name (value-contract val))))
+                     (displayln ctc-msg)
+                     val]
+                    [else val])
+              val)]
+         [form #'form]))
+     (transform expanded-stx))))
